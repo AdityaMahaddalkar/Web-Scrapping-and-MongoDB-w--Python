33fce0a735c3f3de5ecbd5055314eb21a8e1c261
Aditya Mahaddalkar <adityam1311@gmail.com>
2018-08-28 07:55:25 +0530

    Main Program
    Main loop of the whole system.
    Note : DONOT ISSUE MULTIPLE REQUESTS IN VERY SMALL AMOUNT OF TIME. IT IS CALLED SPAMMING.
    
    One request per 5 - 10 minutes is ok.

---
+++ program.py
@@ -0,0 +1,38 @@
+from scrapper import Scraps
+from data.connect import global_init
+from data.weather import Weather
+import mongoengine
+import time
+
+def main():
+    # Connect to database
+
+    global_init('Weather_Database')
+
+
+    while (True):
+
+
+
+        #Scrap the web page
+        ## Warning : Donot issue huge amount of requests in small time
+        ## This is called spamming
+        sc = Scraps()
+        sc.extract()
+
+        # Assign document paramenters
+        w = Weather()
+        w.temperature = sc.temperature
+        w.precipitation = sc.precipitation
+        w.humidity = sc.humidity
+        w.wind = sc.wind
+
+        # save in database
+
+        w.save()
+
+        # wait for n seconds
+        time.sleep(10)
+
+if __name__ == '__main__':
+    main()
